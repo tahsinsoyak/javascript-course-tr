0bcc5b00e55d27043bbf64929c32806e7bd10a60
tahsinsoyak <54424377+tahsinsoyak@users.noreply.github.com>
2021-08-26 17:35:12 +0300

    Add files via upload

---
+++ 05_fractions.js
@@ -0,0 +1,15 @@
+/* Toplama işlemi [add ] */
+var sum = 10+10;
+console.log(sum);
+
+/* Çıkarma işlemi [subtract] */
+var difference = 45 - 33;
+console.log(difference);
+
+/* Çarpma işlemi [multiply] */
+var product = 8 * 80;
+console.log(product);
+
+/* Bölme işlemi [divide] */
+var quotient = 66 / 22;
+console.log(quotient);
