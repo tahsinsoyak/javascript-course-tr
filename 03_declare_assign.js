f7eeaec4abd35799aca03257bfc4dfda73c5ce67
tahsinsoyak <54424377+tahsinsoyak@users.noreply.github.com>
2021-08-26 17:33:49 +0300

    Add files via upload

---
+++ 03_declare_assign.js
@@ -0,0 +1,26 @@
+
+//declare variable, veri tanımladık.
+var a;
+
+//assign variable, veri atadık.
+var b = 2;
+console.log(a);
+
+//tanımladığımıza atama yaptık
+a = 7;
+
+//declearing and initializing
+b = a;
+
+//komut satırına yazdırma
+console.log(a);
+
+
+//örnekler
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+a = a + 1;
+b = b + 5;
+c = c + " String!"
