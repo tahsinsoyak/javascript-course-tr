104d2f88aec0b3395202c5368d663319274a3108
tahsinsoyak <54424377+tahsinsoyak@users.noreply.github.com>
2021-08-26 17:34:25 +0300

    Add files via upload

---
+++ 04_sensitivity_in_variables.js
@@ -0,0 +1,10 @@
+// Tanımlamalar
+var StUdLyCapVar; //bu tanımlama alttaki atama veri tipi ile aynı değil
+var properCamelCase; //doğru kullanım şekli bu olmalıdır
+var TitleCaseOver;
+
+
+// Atamalar
+studlyCapVar = 10;
+PRoperCamelCase = "A String";
+tITLEcASEoVER = 9000;
