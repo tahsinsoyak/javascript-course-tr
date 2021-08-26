7eef97e6e28ec7a03dde08051da79aa94ea35b56
tahsinsoyak <54424377+tahsinsoyak@users.noreply.github.com>
2021-08-26 17:33:07 +0300

    Add files via upload

---
+++ 02_data_types.js
@@ -0,0 +1,26 @@
+/*
+Data Types;
+undefined => tanımlanmamış veri tipi [başında veri tanımı yok]
+null => boş veri tipi [nothing]
+boolean => true false değerli veri tip
+string => yazı değeri veri tipi
+symbol => unique veri tipi
+number => sayı veri tipi
+object => nesne veri tipi
+*/
+
+/* VAR */
+//veri tanımlamak için var => variable [genelde string]
+//daha sonra veri tipi değişebilir.
+//bütün program boyunca kullanılır.
+
+var myName = "Tahsin";
+myName = 8;
+
+/* LET */
+//sadece tanımladığımız aralıkta kullanılır.
+let ourName = "Suntheo";
+
+/* CONST */
+//sadece bir kez tanımlanır, değiştirilmez.
+const pi = 3.14;
