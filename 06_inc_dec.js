ad915cad489e5bc0040b429d56901ccf4e22ae1e
tahsinsoyak <54424377+tahsinsoyak@users.noreply.github.com>
2021-08-27 21:48:28 +0300

    Add files via upload

---
+++ 06_inc_dec.js
@@ -0,0 +1,17 @@
+/* Arttırma işlemi [increment] */
+var myVar = 67;
+
+//iki farklı arttırma yolu
+myVar = myVar + 1;
+myVar++;
+
+console.log(myVar); //iki kez arttırdık
+
+/* Azaltma işlemi [decrement] */
+myVar = 11;
+
+//iki farklı azaltma yolu
+myVar = myVar - 1;
+myVar--;
+
+console.log(myVar);
