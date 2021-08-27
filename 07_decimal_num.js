bc44a6318998f9ab747532889c38977c48abf920
tahsinsoyak <54424377+tahsinsoyak@users.noreply.github.com>
2021-08-27 21:49:14 +0300

    Add files via upload

---
+++ 07_decimal_num.js
@@ -0,0 +1,13 @@
+// Decimal sayı tanımladık
+var ourDecimal = 5.7;
+
+//içinde nokta bulunan bütün sayılar decimaldır.
+var myDecimal = 0.000067;
+
+/* Decimal Sayılarda Çarpma */
+var product = 2.0 * 2.6;
+console.log(product);
+
+/* Bölme */
+product = 52 / 24.8;
+console.log(product);
