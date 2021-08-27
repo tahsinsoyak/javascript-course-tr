efcf1eb0a66658e9a20d4bcdaa841c8119ecc278
tahsinsoyak <54424377+tahsinsoyak@users.noreply.github.com>
2021-08-27 21:49:51 +0300

    Add files via upload

---
+++ 09_augmented_fractions.js
@@ -0,0 +1,13 @@
+/* Arttırılmış İşlemler */
+var a = 3;
+var b = 17;
+var c = 12;
+
+//3. bir ekleme yöntemi olarak işlemi yapıp eşitliyoruz
+a += 12;
+b %= 3; //17 mod 3 yaparak kalan 2'yi b değerine eşitledik
+c -=1;
+c *=4;
+c /=11;
+
+console.log(a,b,c);
